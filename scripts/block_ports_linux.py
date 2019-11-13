c35be04172474e6daf7dd5ee1df8e393c055fa92
Guillermo <98gberasategui@gmail.com>
2019-11-13 20:46:40 +0100

    v1

--- scripts/block_ports_linux.py
+++ scripts/block_ports_linux.py
@@ -19,38 +19,16 @@ def block_some_ports():
         p1 += port + ','
     p1 += arr_puertos[-1]
     comando = p1 + p2
-    print(comando)
     os.system(comando)
+    os.system("sudo service iptables save")
+    os.system("sudo service iptables restart")
     return 
-
-def block_extrem():
-    print("AVISO: En este modo selecciona los puertos que deseas mantener abiertos y el resto seran cerrados (default deny)")
-    print("En caso de querer volver abrir todos los puertos ejecutar $iptables -I INPUT -j ACCEPT")
-    print("Indique los puertos que desee mantener abiertos separados por comas")
-    print("ejemplo: 22, 53, 80")
-    in_puertos = input()
-    arr_puertos = in_puertos.split(",")
-    arr_puertos = [port.replace(" ", "") for port in arr_puertos] #eliminar espacios en blanco
-    p1 = "sudo iptables -A INPUT -p tcp --dport "
-    p2 = " -j ACCEPT"
-    for puerto in arr_puertos:
-        auxp1 = p1
-        auxp2 = p2
-        comando = p1 + puerto + p2
-        os.system(comando)
-    final = "sudo iptables -P INPUT DROP"
-    os.system(final)
-    return 
-
     
-
 def main():
-    block_all_ports()
     print("Script para configuar los puertos los puertos bloqueados por sshguard")
     print("Seleccione una opcion: ")
     print("1 - Proteger todos los puertos")
     print("2 - Elegir los puertos a seleccionar")
-    print("3 - Seguridad extrema")
     eleccion = input()
     while not eleccion.isdigit():
         print("Por favor introduce un numero")
@@ -67,8 +45,6 @@ def main():
         block_all_ports()
     elif eleccion==2:
         block_some_ports()
-    else:
-        block_extrem()
     print("\n\n¡Puertos asegurados, puedes utilizar tu pc tranquilo!")
 
 main()
